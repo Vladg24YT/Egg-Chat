05db0ee4c31dbe62e6890cbea7b3fa0fd9e40dfe
Владислав Горский <vg07102004@gmail.com>
2023-03-12 09:03:32 +0300

    Update functions.cpp
    a lil' bit of grammar

--- parser/src/functions.cpp
+++ parser/src/functions.cpp
@@ -2,13 +2,13 @@
 
 // функции пустышки
 void signInAdmin(std::string name, std::string password){
-    std::cout << "Admin logines with " << name << ":" << password << "\n";
+    std::cout << "Admin login as " << name << ":" << password << "\n";
 }
 void signInUser(std::string name, std::string password){
-    std::cout << "User logines with " << name << ":" << password << "\n";
+    std::cout << "User login as " << name << ":" << password << "\n";
 }
 void registration(std::string name, std::string password){
-    std::cout << "New user registred with " << name << ":" << password << "\n";
+    std::cout << "New user registered with " << name << ":" << password << "\n";
 }
 void sendMessage(std::string chatID, std::string text){
     std::cout << "User sent message \"" << text << "\" to chat " << chatID <<"\n";
@@ -36,7 +36,7 @@ void answerInvite(std::string answer, std::string chatID){
         std::cout << "accepted ";
     else
         std::cout << "denied ";
-    std::cout << "invite to chatID = " << chatID << "\n";
+    std::cout << "the invite to chatID = " << chatID << "\n";
 }
 
 void kickUserFromChat(std::string userID, std::string chatID){
