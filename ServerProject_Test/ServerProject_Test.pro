2a0e3e70d06e1a2d11b96a45bb0beec48bf047a3
Руслан Асадов <rustv2002@mail.ru>
2023-06-01 22:59:50 +0300

    Доработаны тесты

--- ServerProject_Test/ServerProject_Test.pro
+++ ServerProject_Test/ServerProject_Test.pro
@@ -7,11 +7,13 @@ CONFIG -= app_bundle
 TEMPLATE = app
 
 SOURCES +=  tst_parsertestcase.cpp \
+    ../AlphaDesign/chat.cpp \
     ../AlphaDesign/invite.cpp \
     ../ServerProject/client.cpp \
     ../ServerProject/dbworker.cpp
 
 HEADERS += \
+    ../AlphaDesign/chat.h \
     ../AlphaDesign/invite.h \
     ../ServerProject/DBWorker.h \
     ../ServerProject/client.h
--- ServerProject_Test/tst_parsertestcase.cpp
+++ ServerProject_Test/tst_parsertestcase.cpp
@@ -1,6 +1,7 @@
 #include <QtTest>
 #include "../ServerProject/DBWorker.h"
 #include "../AlphaDesign/invite.h"
+#include "../AlphaDesign/chat.h"
 
 // add necessary includes here
 
@@ -30,19 +31,19 @@ ParserTestCase::~ParserTestCase(){
 }
 
 void ParserTestCase::test_case1(){
-   QVERIFY2(true == DBWorker::getInstance()->insertUser("", "12345", "email@email.net"), "Отсутсвует логин!");
+   QVERIFY2(true != DBWorker::getInstance()->insertUser("", "12345", "email@email.net"), "Отсутсвует логин!");
 }
 void ParserTestCase::test_case2(){
 
-   QVERIFY2(true == DBWorker::getInstance()->searchUser("старый дед"), "Пользователь не найден!");
+   QVERIFY2(true == DBWorker::getInstance()->searchUser("old"), "Пользователь не найден!");
 }
 void ParserTestCase::test_case3(){
     invite inv = invite("499", "funny chat 23", "75", "user128457");
     QVERIFY2("invite|answer|499|75|1" == inv.answer(true), "Ошибка генерации команды!");
 }
 void ParserTestCase::test_case4(){
-
-   QVERIFY2("true" == "МЕТОД КЛИЕНТА", "ТЕКСТ ОШИБКИ");
+    message msg = message("Тучка", "Привет мир!", "01.01.2023");
+    QVERIFY("[Тучка] Привет мир!\n (01.01.2023)" == msg.show());
 }
 
 QTEST_APPLESS_MAIN(ParserTestCase)
